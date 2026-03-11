d83bc3e2f26df7fa77cab09b9986156be8a64e32
Justin Chung <easeluease@users.noreply.github.com>
2026-03-11 00:31:54 -0700

    added vercel analytics

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,6 +3,7 @@ import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 
 import { NavbarDemo } from "@/components/navbar";
+import { Analytics } from "@vercel/analytics/next";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -33,6 +34,7 @@ export default function RootLayout({
             <NavbarDemo />
             {children}
           </div>
+          <Analytics />
       </body>
     </html>
   );
