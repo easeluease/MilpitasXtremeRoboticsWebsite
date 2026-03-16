6f19cdb9af725209a7201afb06f0902b4aa9161c
Justin Chung <easeluease@users.noreply.github.com>
2026-03-15 17:38:26 -0700

    resolved Next.js build warning

--- next.config.ts
+++ next.config.ts
@@ -3,10 +3,12 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   /* config options here */
   images: {
-    domains: ["api.microlink.io"],
-  },
-  eslint: {
-    ignoreDuringBuilds: true,
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "api.microlink.io",
+      },
+    ],
   },
   typescript: {
     ignoreBuildErrors: true,
